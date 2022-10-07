95491d1b2ada303bb8e8e4a417e480fcbd57ff3f
dustein <steindu@gmail.com>
2022-10-07 16:23:20 -0300

    primeiras rotas criadas

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,19 @@ app.get('/', (req, res) => {
    res.json({message: "pasta raiz"})
 });
 
+app.get('/users', async (req, res) => {
+
+   const listaUsers = await prisma.user.findMany();
+
+   res.status(200).send(listaUsers)
+})
+
+app.get('/jobs', async (req, res) => {
+
+   const listaJobs = await prisma.job.findMany();
+
+   res.status(200).send(listaJobs)
+})
 
 
 app.listen(3030, () => { console.log('Servidor OK porta 3030')})
